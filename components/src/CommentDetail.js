7867689473451f7e8fc3160a9913f124b346e9f8
Will <Will.irvin18@gmail.com>
2020-08-25 13:08:56 -0400

    messing with props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img src={props.photo} />
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam
-                  </a>
+                    {props.author}
+                </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice Blog post!!</div>
+                <div className="text">{props.comment}</div>
             </div>
         </div>
     );
--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                comment="Good Job bro!"
+                photo={faker.image.avatar()} />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00AM"
+                comment="this blog post changed my life and my wifes life"
+                photo={faker.image.avatar()} />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Yesterday at 4:30PM"
+                comment="I dont know what I did before reading this"
+                photo={faker.image.avatar()} />
         </div>
 
     );
